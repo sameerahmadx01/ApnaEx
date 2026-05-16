1d74526987e5c9ecf0a3061228583de6d90285c4
Ahmad <monyeran6377@gmail.com>
2026-05-16 13:24:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from flask import Flask
 import threading
-from bot import Bot   # tumhara bot module
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running!"
+    return "Extractor bot is running!"
 
 def run_bot():
-    bot = Bot()
-    bot.run()
+    os.system("python -m Extractor")
 
 if __name__ == "__main__":
-    # Bot ko background thread me start karo
     threading.Thread(target=run_bot).start()
-    # Flask ko port 10000 pe run karo
     app.run(host="0.0.0.0", port=10000)
