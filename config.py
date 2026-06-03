d2fb4b4e8651368f870e2e9d08e2f60432bfb36c
Ahmad <monyeran6377@gmail.com>
2026-06-03 20:58:33 +0530

    Refactor config.py for better organization

--- config.py
+++ config.py
@@ -1,40 +1,27 @@
 import os
 
-# ------------------------------------------------
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-# ------------------------------------------------
-BOT_USERNAME = "@Sadudi_bot"   # public handle, safe
-BOT_TEXT = "GAJJU BHAI KA EXTRACTOR" # display text, safe
-# ------------------------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID"))
-# ------------------------------------------------
-# LOG CHANNEL IDs
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID"))
-CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2"))
-# ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL")
-# ------------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS"))
-# ------------------------------------------------
-join = '<a href="https://t.me/RPSC_UPSC_CURRENT">✳️ JOIN BACKUP</a>'  # public link, safe
-# ------------------------------------------------
-UNSPLASH_ACCESS_KEY = os.environ.get("UNSPLASH_ACCESS_KEY")  # shift to env if personal key
-UNSPLASH_QUERY = 'animal baby'  # safe
-# ------------------------------------------------
-ADMIN_BOT_USERNAME = "Mirza_Ghallib"  # safe
-# ------------------------------------------------
-THUMB_URL = os.environ.get("THUMB_URL")
-# # Bot configuration
-# API_ID = int(os.environ.get("API_ID", "21157244"))
-# API_HASH = os.environ.get("API_HASH", "4981c2699bd91c7db836ec8f77e5b0f0")
-# BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-# BOT_USERNAME = os.environ.get("BOT_USERNAME", "Gajjubhaikaext_bot")
-# OWNER_ID = int(os.environ.get("OWNER_ID", "1783306092"))
-# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1783306092").split()))
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003913975911"))
-# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002195125473"))
-# THUMB_URL = os.environ.get("THUMB_URL", "https://i.fbcd.co/products/original/ug-circle-logo-design-2-e84695ca2ab9a697d2b2d7c928b0bf5f12bf18e076da241815e0372c8d617915.jpg")
 
+BOT_USERNAME = "@Sadudi_bot"
+BOT_TEXT = "GAJJU BHAI KA EXTRACTOR"
+
+OWNER_ID = int(os.environ.get("OWNER_ID"))
+
+# LOG CHANNEL IDs
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003913975911"))
+CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1003913975911"))
+
+MONGO_URL = os.environ.get("MONGO_URL")
+
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003913975911"))
+
+join = '<a href="https://t.me/RPSC_UPSC_CURRENT">✳️ JOIN BACKUP</a>'
+
+UNSPLASH_ACCESS_KEY = os.environ.get("UNSPLASH_ACCESS_KEY")
+UNSPLASH_QUERY = 'animal baby'
+
+ADMIN_BOT_USERNAME = "Mirza_Ghallib"
+
+THUMB_URL = os.environ.get("THUMB_URL")
